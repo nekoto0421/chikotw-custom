89f34bf5ff71bb0a29ddf651d9719653dbba326b
nekoto0421 <johnny5610j67k601@gmail.com>
2023-06-27 10:56:32 +0800

    Update album.js

--- js/album.js
+++ js/album.js
@@ -1,32 +1,33 @@
 var updateAlbum="";
 var oldAlbum="";
 jQuery(function($) {
-  $( document ).ready(function() {
-    if ($(window).width() >= 850) {
-      resetAlbum();
-    }
-  })
-  function resetAlbum(){
-    let i=1;
-    $(".hp-grid__item img").each(function(){
-      if(i<=5){
-        if($(this).attr("src")!=""){
-          $("#image"+i).attr("src",$(this).attr("src"));
-        }
-        else{
-          $("#image"+i).attr("src","https://si.chikotw.com/wp-content/uploads/2023/05/社區預設圖.jpg");
-        }
-      }
-      i++;
-    });
-    
-    $(".custom-img").each(function(){
-      if($(this).attr("src")==""){
-        $(this).attr("src","https://si.chikotw.com/wp-content/uploads/2023/05/社區預設圖.jpg");
-      }
-    });
-    
-    $(".hp-listing__images").remove();
-    $(".hp-page__topbar").after($(".custom-track-out").html());
-  }
+	$( document ).ready(function() {
+		if ($(window).width() >= 850) {
+			resetAlbum();
+		}
+	})
+	function resetAlbum(){
+		let i=1;
+		$(".hp-page__header .hp-grid__item img").each(function(){
+			if(i<=5){
+				console.log("imglog"+$(this).attr("src"));
+				if($(this).attr("src")!=""){
+					$("#image"+i).attr("src",$(this).attr("src"));
+				}
+				else{
+					$("#image"+i).attr("src","https://si.chikotw.com/wp-content/uploads/2023/05/社區預設圖.jpg");
+				}
+			}
+			i++;
+		});
+		
+		$(".custom-img").each(function(){
+			if($(this).attr("src")==""){
+				$(this).attr("src","https://si.chikotw.com/wp-content/uploads/2023/05/社區預設圖.jpg");
+			}
+		});
+		
+		$(".hp-listing__images").remove();
+		$(".hp-page__topbar").after($(".custom-track-out").html());
+	}
 })
